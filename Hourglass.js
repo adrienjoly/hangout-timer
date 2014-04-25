e5b43158de7a7ebef46dbf22f8c051e97f3dd83e
Adrien Joly <adrien.joly@gmail.com>
2014-04-25 17:20:58 +0200

    Hourglass: simple vertical progressbar, Stopwatch: simple timer

--- Hourglass.js
+++ Hourglass.js
@@ -3,40 +3,50 @@ function Hourglass(canvas){
 	var w = canvas.width, h = canvas.height;
 	console.log("Hourglass canvas size", w, h)
 	function drawTimer(ratio) {
-		/*
-		ctx.clearRect(0, 0, w, h);
-		console.log("clean");
-		console.log(canvas.toDataURL());
-		ctx.beginPath();
-		ctx.moveTo(0, 0);
-		ctx.lineTo(w/2, h/2);
-		ctx.moveTo(w/2, 0);
-		ctx.lineTo(0, h/2);
-		ctx.stroke();
-		console.log("lines");
-		console.log(canvas.toDataURL());
-		*/
+		//ctx.clearRect(0, 0, w, h);
 		ctx.fillStyle = 'white';
 		ctx.fillRect(w/2 - 10, 10, 20, 40);
 		ctx.fillStyle = 'blue';
 		ctx.fillRect(w/2 - 9, 11, 18, (ratio * 38));
-		/*
-		ctx.fillStyle = 'red';
-		ctx.fillRect(10, 10, 20, 20);
-		ctx.fillStyle = 'green';
-		ctx.fillRect(w-30, h-30, 20, 20);
-		*/
-		console.log("rects");
-		console.log(canvas.toDataURL());
 	}
 	return {
 		drawToDataUrl: function(ratio){
-			try {
-				drawTimer(ratio);
-			}catch(e){
-				console.error(e, e.stack);
-			}
+			drawTimer(ratio);
 			return canvas.toDataURL();
 		}
 	};
 };
+
+function Stopwatch(){
+	return {
+		t0: null,
+		elapsed: null,
+		duration: 30000,
+		interval: null,
+		handler: null,
+		stop: function(){
+			if (this.interval) {
+				clearInterval(this.interval);
+				this.interval = null;
+			}
+		},
+		start: function(){
+			this.stop();
+			//if (this.onTick) {
+				this.t0 = Date.now();
+				var that = this;
+				this.interval = setInterval(function(){
+					that.elapsed = Date.now() - that.t0;
+					if (that.handler)
+						that.handler(that);
+					if (that.elapsed >= that.duration)
+						that.stop();
+				}, 200);
+			//}
+		},
+		reset: function(duration){
+			this.stop();
+			this.duration = duration;
+		}
+	};
+}
