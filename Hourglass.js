a9c211df9f169e575b8ad66ced5c400543e9d3e7
Adrien Joly <adrien.joly@gmail.com>
2014-04-25 17:38:28 +0200

    Hourglass now looks like a clock.

--- Hourglass.js
+++ Hourglass.js
@@ -1,5 +1,7 @@
 function Hourglass(canvas){
 	var ctx = canvas.getContext("2d");
+	var r = Math.min(canvas.width, canvas.height) / 2;
+	/*
 	var w = canvas.width, h = canvas.height;
 	console.log("Hourglass canvas size", w, h)
 	function drawTimer(ratio) {
@@ -8,6 +10,14 @@ function Hourglass(canvas){
 		ctx.fillRect(w/2 - 10, 10, 20, 40);
 		ctx.fillStyle = 'blue';
 		ctx.fillRect(w/2 - 9, 11, 18, (ratio * 38));
+	}
+	*/
+	function drawTimer(ratio) {
+		var angle = Math.PI * 2 * ratio - Math.PI / 2;
+		ctx.beginPath();
+		ctx.moveTo(r, r);
+		ctx.lineTo(r + r * Math.cos(angle), r + r * Math.sin(angle));
+		ctx.stroke();
 	}
 	return {
 		drawToDataUrl: function(ratio){
@@ -31,22 +41,21 @@ function Stopwatch(){
 			}
 		},
 		start: function(){
+			var that = this;
 			this.stop();
-			//if (this.onTick) {
-				this.t0 = Date.now();
-				var that = this;
-				this.interval = setInterval(function(){
-					that.elapsed = Date.now() - that.t0;
-					if (that.handler)
-						that.handler(that);
-					if (that.elapsed >= that.duration)
-						that.stop();
-				}, 200);
-			//}
+			this.t0 = Date.now();
+			this.interval = setInterval(function(){
+				that.elapsed = Date.now() - that.t0;
+				if (that.handler)
+					that.handler(that);
+				if (that.elapsed >= that.duration)
+					that.stop();
+			}, 1);
 		},
 		reset: function(duration){
 			this.stop();
 			this.duration = duration;
+			return this;
 		}
 	};
 }
